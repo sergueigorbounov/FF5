0acb6d220f4b0d5522b8de5ffb43717a35311a6e
sergueigorbounov <127092671+sergueigorbounov@users.noreply.github.com>
2024-04-09 22:24:19 +0200

    next

--- src/main/java/com/example/ff/model/Cell.java
+++ src/main/java/com/example/ff/model/Cell.java
@@ -4,8 +4,8 @@ public class Cell {
     private boolean burning;
     private boolean burned;
     private boolean tree;
-    // Constructor for creating a Cell with only its burning state specified.
-    // The burned state defaults to false.
+    // Constructor for creating a Cell with only its burning state specified
+    // The burned state defaults to false tree also false
     public Cell(boolean isBurning) {
         this.burning = isBurning;
         this.burned = false; // Initially, a cell isn't burned.
@@ -27,19 +27,19 @@ public class Cell {
     // Sets the cell's burning state.
     public void setBurning(boolean burning) {
         this.burning = burning;
-        // If a cell is set to burning, then it's also considered burned regardless of the future state.
+        // If a cell is set to burning, then it's also considered burned regardless of the future state
         if (burning) {
             this.burned = true;
             this.tree = false;
         }
     }
 
-    // Returns true if the cell has been burned at any point.
+    // Returns true if the cell has been burned at any point
     public boolean isBurned() {
         return burned;
     }
 
-    // Sets the cell's burned state.
+    // Sets the cell's burned state
     // Note: This method could potentially contradict the natural progression of the cell's state
     // if not used carefully. A cell shouldn't typically be "unburned".
     public void setBurned(boolean burned) {
